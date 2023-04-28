0d27274ec89a9f4e75d6b81db37d20f2fa3c0913
Avgerinos Xristos <xristoskitharistas@gmail.com>
2023-04-28 19:24:17 +0300

    changes

--- app/src/main/java/com/unipi/chrisavg/eventity/SignUpActivity.java
+++ app/src/main/java/com/unipi/chrisavg/eventity/SignUpActivity.java
@@ -1,14 +1,93 @@
 package com.unipi.chrisavg.eventity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText email,fullname,password,confirmPassword;
+    Toolbar toolbar;
+    TextInputLayout passwordL,confirmPasswordL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        email= findViewById(R.id.et_email);
+        fullname=findViewById(R.id.et_fullname);
+        password=findViewById(R.id.et_password);
+        confirmPassword=findViewById(R.id.et_confirm_password);
+
+        passwordL = findViewById(R.id.passwordLayout);
+        confirmPasswordL = findViewById(R.id.confPasswordLayout);
+
+
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // getSupportActionBar().hide(); //hide the title bar
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignUpActivity.super.onBackPressed();
+            }
+        });
+        setStatusBarWhite(this);
+
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                passwordL.setPasswordVisibilityToggleEnabled(true);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        confirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                confirmPasswordL.setPasswordVisibilityToggleEnabled(true);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
+    private void setStatusBarWhite(AppCompatActivity activity){
+        //Make status bar icons color dark
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            activity.getWindow().setStatusBarColor(Color.WHITE);
+        }
+    }
+
 }
