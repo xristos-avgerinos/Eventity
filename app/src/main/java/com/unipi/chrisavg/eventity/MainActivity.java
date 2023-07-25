8faee02ab465b50d0e2b267bf72214d1a3159197
Avgerinos Xristos <xristoskitharistas@gmail.com>
2023-07-25 19:19:46 +0300

    Hobby Selection

--- app/src/main/java/com/unipi/chrisavg/eventity/MainActivity.java
+++ app/src/main/java/com/unipi/chrisavg/eventity/MainActivity.java
@@ -1,112 +1,82 @@
 package com.unipi.chrisavg.eventity;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.Menu;
+import android.widget.TextView;
 
 import com.facebook.login.LoginManager;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.ktx.Firebase;
-
-import java.util.HashMap;
+import com.unipi.chrisavg.eventity.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     FirebaseAuth mAuth;
     CollectionReference users;
     FirebaseFirestore db;
-    EditText email,password;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Initialize Firebase Auth
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_tickets, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         users = db.collection("Users");
 
-        email = findViewById(R.id.et_email);
-        password = findViewById(R.id.et_password);
+        // Change the subtitle of the nav_header_main
+        TextView textViewHeaderTitle = navigationView.getHeaderView(0).findViewById(R.id.textViewHeaderTitle);
+        textViewHeaderTitle.setText(mAuth.getCurrentUser().getDisplayName());
 
-
+        // Change the subtitle of the nav_header_main
+        TextView textViewHeaderSubtitle = navigationView.getHeaderView(0).findViewById(R.id.textViewHeaderSubtitle);
+        textViewHeaderSubtitle.setText(mAuth.getCurrentUser().getEmail());
     }
 
-    public void go(View view){
-        mAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString())
-                .addOnCompleteListener((task)->{
-                    if(task.isSuccessful()){
 
-                       /*User user = new User("1234567");
-                       user.setAge(13);
-                       user.setName("chris");
-                        HashMap<String, Boolean> preferences = new HashMap<String, Boolean>();
-                        preferences.put("music",true);
-                        preferences.put("travel",false);
-                        preferences.put("arts",true);
-                        preferences.put("sports",true);
-                       user.setPreferences(preferences);
-
-                        users.document(mAuth.getUid())
-                                .set(user)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Toast.makeText(getApplicationContext(), "written to db"+ user.getToken(), Toast.LENGTH_SHORT).show();
-
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.w(TAG, "Error writing document", e);
-                                    }
-                                });*/
-
-                        users.document(mAuth.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                            @Override
-                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                if(documentSnapshot.exists()){
-                                    User user = documentSnapshot.toObject(User.class);
-
-                                }else{
-                                    Toast.makeText(getApplicationContext(), "failed to find userId", Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-
-                        });
-
-
-                    }else{
-                        Toast.makeText(this, "notttt found on auth", Toast.LENGTH_SHORT).show();
-                    }
-                });
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
+
     public void maps(View view){
         Intent maps = new Intent(this,MapsActivity.class);
         startActivity(maps);
@@ -149,11 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-
-
-
         }
 
     }
-
 }
