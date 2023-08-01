094971f8ccbd153d70dfd2b408e0f457e37f5503
Avgerinos Xristos <xristoskitharistas@gmail.com>
2023-08-01 19:57:36 +0300

    search places - location

--- app/src/main/java/com/unipi/chrisavg/eventity/EventsSearchActivity.java
+++ app/src/main/java/com/unipi/chrisavg/eventity/EventsSearchActivity.java
@@ -1,30 +1,11 @@
 package com.unipi.chrisavg.eventity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
-
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.AutocompletePrediction;
-import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.api.net.FetchPlaceRequest;
-import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 
 public class EventsSearchActivity extends AppCompatActivity {
 
@@ -34,7 +15,7 @@ public class EventsSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_search);
 
-        LinearLayout customButton = findViewById(R.id.customButton);
+        LinearLayout customButton = findViewById(R.id.CurrentLocationButton);
         customButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
