9689b5fc945d51865d70421f90397fe3a4eb1607
Avgerinos Xristos <xristoskitharistas@gmail.com>
2023-08-03 12:16:39 +0300

    search places - location

--- app/src/main/java/com/unipi/chrisavg/eventity/EventsSearchActivity.java
+++ app/src/main/java/com/unipi/chrisavg/eventity/EventsSearchActivity.java
@@ -2,29 +2,43 @@ package com.unipi.chrisavg.eventity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class EventsSearchActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.unipi.chrisavg.eventity.databinding.ActivityMapsBinding;
 
+public class EventsSearchActivity extends AppCompatActivity{
+
+    String imageUrl = "https://cdn-az.allevents.in/events7/banners/61ec31a907e18ae6aec7baacdecc6e2e91f36dfaeb5bb3c398141f24f3c5014c-rimg-w960-h503-gmir.jpg?v=1690810850"; // Replace with your image URL
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_search);
 
-        LinearLayout customButton = findViewById(R.id.CurrentLocationButton);
-        customButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // This code will be executed when the custom button is clicked.
-                // You can perform any actions you want here.
-                // For example, show a toast, start another activity, etc.
-                Toast.makeText(EventsSearchActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+        ImageView imageView = findViewById(R.id.imageView);
+        Glide.with(this)
+                .load(imageUrl)
+                .diskCacheStrategy(DiskCacheStrategy.ALL) // Cache the image for better performance
+                .into(imageView);
+
+    }
+
+    public void maps(View view){
+        Intent intent = new Intent(EventsSearchActivity.this,MapsActivity.class);
+        startActivity(intent);
     }
 
 
