a311ce4dcd689d070b0044f15ce1d2973e8836ed
Avgerinos Xristos <xristoskitharistas@gmail.com>
2023-08-13 00:04:02 +0300

    tab 2

--- app/src/main/java/com/unipi/chrisavg/eventity/ui/EventsSearch/Tab2Fragment.java
+++ app/src/main/java/com/unipi/chrisavg/eventity/ui/EventsSearch/Tab2Fragment.java
@@ -1,10 +1,14 @@
 package com.unipi.chrisavg.eventity.ui.EventsSearch;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -12,6 +16,7 @@ import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,15 +29,23 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.maps.android.clustering.Cluster;
+import com.google.maps.android.clustering.ClusterItem;
 import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.algo.Algorithm;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
 import com.unipi.chrisavg.eventity.Event;
 import com.unipi.chrisavg.eventity.MyClusterItem;
 import com.unipi.chrisavg.eventity.R;
@@ -40,11 +53,12 @@ import com.unipi.chrisavg.eventity.User;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 
-public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
+public class Tab2Fragment extends Fragment implements OnMapReadyCallback, ClusterManager.OnClusterClickListener<MyClusterItem>, ClusterManager.OnClusterItemClickListener<MyClusterItem> {
 
     private GoogleMap mMap;
     static final int LOCATION_SETTINGS_REQUEST = 1;
@@ -64,6 +78,7 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
 
     Location locationForSearch;
 
+    ClusterManager<MyClusterItem> clusterManager;
 
 
     @Nullable
@@ -99,6 +114,7 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
         auth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         events = db.collection("Events");
+
 
 
 
@@ -166,6 +182,18 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
 
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        clusterManager = new ClusterManager<MyClusterItem>(getContext(), mMap);
+
+        ClusterItemRenderer clusterItemRenderer = new ClusterItemRenderer();
+        clusterManager.setRenderer(clusterItemRenderer);
+        //mMap.setOnCameraChangeListener((GoogleMap.OnCameraChangeListener) clusterManager);
+        mMap.setOnCameraIdleListener(clusterManager);
+        mMap.setOnMarkerClickListener(clusterManager);
+        mMap.setOnInfoWindowClickListener(clusterManager);
+        clusterManager.setOnClusterClickListener(this);
+        clusterManager.setOnClusterItemClickListener(this);
+
+
 
 
         // Fetch user from the Users collection
@@ -216,6 +244,8 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
                                         // Here, "userList" contains the updated data with real-time changes
                                         // You can now use the "userList" in your app
                                         ShowEventsOnMap();
+                                        clusterManager.cluster();
+
                                     });
                         }
                     }
@@ -224,11 +254,8 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
 
     }
 
-    public void ShowEventsOnMap(){
 
-        ClusterManager<MyClusterItem> clusterManager = new ClusterManager<MyClusterItem>(getContext(), mMap);
-        mMap.setOnCameraIdleListener(clusterManager);
-        mMap.setOnMarkerClickListener(clusterManager);
+    public void ShowEventsOnMap(){
 
         for (Event event:eventsList) {
             // Adding markers
@@ -241,11 +268,7 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
 
         }
 
-        LatLng latLng = new LatLng(40.638366, 22.971797);
-        MyClusterItem clusterItem = new MyClusterItem(latLng, "title", "title");
-        clusterManager.addItem(clusterItem);
-
-        LatLng latLng2 = new LatLng(37.808647, 23.790229);
+     /*   LatLng latLng2 = new LatLng(37.808647, 23.790229);
         MyClusterItem clusterItem2 = new MyClusterItem(latLng2, "title", "title");
         clusterManager.addItem(clusterItem2);
 
@@ -255,19 +278,9 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
 
         LatLng latLng4 = new LatLng( 38.000636, 23.985542);
         MyClusterItem clusterItem4 = new MyClusterItem(latLng4, "title", "title");
-        clusterManager.addItem(clusterItem4);
+        clusterManager.addItem(clusterItem4);*/
 
-        LatLng latLng5 = new LatLng(38.525218, 23.867792);
-        MyClusterItem clusterItem5 = new MyClusterItem(latLng5, "title", "title");
-        clusterManager.addItem(clusterItem5);
-
-        LatLng latLng6 = new LatLng( 38.667766, 23.619803);
-        MyClusterItem clusterItem6 = new MyClusterItem(latLng6, "title", "title");
-        clusterManager.addItem(clusterItem6);
-
-        /*mMap.setOnMarkerClickListener(clusterManager);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.5145160,-0.1270060),13));
-        mMap.setOnCameraIdleListener(clusterManager);*/
+        zoomToCenterOfClusterItems();
 
     }
 
@@ -275,7 +288,9 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
         for (Marker marker : markerList) {
             marker.remove();
         }
-        markerList.clear(); // Clear the list of markers
+        clusterManager.clearItems(); // Clear the cluster items from the data set
+        clusterManager.cluster(); // Recalculate and render the clusters
+
     }
 
     public void showSettingsAlert() {
@@ -291,6 +306,64 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
         alertDialog.show();
     }
 
+    /**
+     * Draws profile photos inside markers (using IconGenerator).
+     * When there are multiple people in the cluster, draw multiple photos (using MultiDrawable).
+     */
+    private class ClusterItemRenderer extends DefaultClusterRenderer<MyClusterItem> {
+        public ClusterItemRenderer() {
+            super(getApplicationContext(), mMap, clusterManager);
+        }
+
+        @Override
+        protected boolean shouldRenderAsCluster(Cluster cluster) {
+            // Always render clusters.
+            return cluster.getSize() > 1;
+        }
+    }
 
 
+    @Override
+    public boolean onClusterClick(Cluster<MyClusterItem> cluster) {
+
+        Collection<MyClusterItem> clusterItems = cluster.getItems();
+
+        // Calculate the center of all cluster items
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (MyClusterItem clusterItem : clusterItems) {
+            builder.include(clusterItem.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        LatLng center = bounds.getCenter();
+
+        // Move the camera to the calculated center with a specific zoom level
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 12.0f));
+        return true;
+    }
+
+    public void zoomToCenterOfClusterItems(){
+        // Get all cluster items from the algorithm
+        Algorithm<MyClusterItem> algorithm = clusterManager.getAlgorithm();
+        Collection<MyClusterItem> clusterItemsCollection = algorithm.getItems();
+
+        // Convert the collection to a list
+        List<MyClusterItem> clusterItems = new ArrayList<>(clusterItemsCollection);
+
+        // Calculate the center of all cluster items
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (MyClusterItem clusterItem : clusterItems) {
+            builder.include(clusterItem.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        LatLng center = bounds.getCenter();
+
+        // Move the camera to the calculated center with a specific zoom level
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 9.0f));
+    }
+
+    @Override
+    public boolean onClusterItemClick(MyClusterItem item) {
+        // Does nothing, but you could go into the user's profile page, for example.
+        return false;
+    }
 }
